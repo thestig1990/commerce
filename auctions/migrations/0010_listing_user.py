ad275024734aee32104fe31cfadf9a3dd52bb181
Yevhen Yakymov <yakymov1yevhen@gmail.com>
2024-04-27 16:50:17 +0300

    updated 0010_listing_user.py file - set default to 1

--- auctions/migrations/0010_listing_user.py
+++ auctions/migrations/0010_listing_user.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='listing',
             name='user',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='listings', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='listings', to=settings.AUTH_USER_MODEL),
         ),
     ]
